a8719fc914f4b3505bfe0c55c2e3d886a393bb9d
Tom Jorquera <tom.jorquera@linagora.com>
2016-04-05 13:49:31 +0200

    [mon] Quickfix for issue #391
    This is a quickfix for issue #391. It *should* not be merged into the
    main branch! It is only a temporary workaround for Y2 review demo.

--- lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/api/impl/utils/RestResource.java
+++ lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/api/impl/utils/RestResource.java
@@ -7,7 +7,7 @@ import org.apache.commons.httpclient.auth.AuthScope;
 
 public class RestResource {
 	final private static String PROTOCOL = "http";
-	final private static String HOSTNAME = "localhost";
+	final private static String HOSTNAME = "192.168.0.100";
 	final private static int PORT = 8080;
 	final private static int SIM_PORT = 8081;
 	final private static String HOST = PROTOCOL + "://" + HOSTNAME + ":" + PORT;
